6f455d5ef2883d36e6dd0d65cb57f70683837aac
Harrison Liddiard <git@harrisonliddiard.com>
2026-08-24 10:54:09 -0400

    style(footer): balance text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 
 const Footer = () => (
-  <footer className="clear-both float-right m-8 max-w-[440px] text-right text-sm opacity-70">
+  <footer className="clear-both float-right m-8 max-w-[440px] text-right text-sm opacity-70 text-balance">
     Content © {new Date().getFullYear()} Harrison Liddiard. All rights reserved.
     Product names, logos, and brands are property of their respective owners.{' '}
     <Link href="/credits">Background image credits</Link>.
