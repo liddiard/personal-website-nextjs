f83fa45c1e3318a7c570a09aa5a94a2808cdd4aa
Harrison Liddiard <git@harrisonliddiard.com>
2026-08-24 16:33:55 -0400

    content(404): update body text wording

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
       <h1 className="text-4xl">Not Found</h1>
       <p className="leading-relaxed">
         Uh oh! That page doesn’t exist. <br />
-        Try one of the links from the menu above.
+        Try a link from the menu above.
       </p>
     </Layout>
   )
